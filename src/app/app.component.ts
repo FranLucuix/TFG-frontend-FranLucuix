6e350d338dca5c2fa662a1ee872e5cd9ca901a77
francisco <gokufcohd@gmail.com>
2025-05-22 21:01:11 +0200

    primer commit frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
+
+import { routes } from './app.routes';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  standalone: true,
+  imports: [
+    RouterModule,   
+    HeaderComponent,
+    FooterComponent,
+  ],
 })
-export class AppComponent {
-  title = 'juliovera';
-}
+export class AppComponent {}
